cc9ad12b48f7eceac2575e510e2281e504f5c257
daikonchan <71866372+daikonchan@users.noreply.github.com>
2020-09-25 20:04:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,6 +29,18 @@ async def on_command_error(ctx, error):
 async def yamaha(ctx):
     await ctx.send('やまはさんはただのJS....')
     
+    
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def taruru(ctx):
+    await ctx.send('ク○ニ所望してるひといますか？')
+    
 
 
 bot.run(token)
