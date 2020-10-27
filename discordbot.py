551d0491159133749f67de2acc6174f84b7e07e3
daikonchan <71866372+daikonchan@users.noreply.github.com>
2020-10-28 08:09:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -65,19 +65,6 @@ async def on_command_error(ctx, error):
 async def kuruchan(ctx):
     await ctx.send('ふごふごふごふごふごふご！ふごふご！！ふごっ！！ふごふご！！')
    
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def akira(ctx):
-    await ctx.send('漆黒極')
-
     
 
 
